c5fec1715e96c7736484fba2371c91dbcd27b146
Alexander Laudino <93599746+alexander-laudino@users.noreply.github.com>
2022-06-13 16:15:38 -0400

    Add loop through characters in string to test for case and non-alphabetical characters

--- caesar-cipher/caesar_cipher.rb
+++ caesar-cipher/caesar_cipher.rb
@@ -3,6 +3,15 @@ require 'pry-byebug'
 def caesar_cipher(string, shift_factor = 3)
   shift_factor = shift_factor % 26
   string = string.split('')
+  string.each do |char| 
+    if ('A'..'Z').include?(char)
+      puts "Capital Letter"
+    elsif ('a'..'z').include?(char)
+      puts "lowercase letter"
+    else
+      puts char
+    end
+  end
   puts "#{string} #{shift_factor}"
 end
 
