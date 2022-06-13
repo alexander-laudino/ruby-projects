e81b37eda62282b91f72e734ccdea5b16337ff49
Alexander Laudino <93599746+alexander-laudino@users.noreply.github.com>
2022-06-13 19:37:33 -0400

    Remove debugger

--- sub-strings/sub_strings.rb
+++ sub-strings/sub_strings.rb
@@ -1,10 +1,7 @@
-require 'pry-byebug'
-
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(string, array = nil)
   hash = array.reduce(Hash.new(0)) do |words, count|
-    #binding.pry
     if string.include?(count)
       words[count] += 1
     end
