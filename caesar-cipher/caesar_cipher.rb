b3c1754d15f60c80a29b6662241172988b8d7965
Alexander Laudino <93599746+alexander-laudino@users.noreply.github.com>
2022-06-13 16:39:42 -0400

    Add variables for encrypted_string and new_char, build string and print

--- caesar-cipher/caesar_cipher.rb
+++ caesar-cipher/caesar_cipher.rb
@@ -3,24 +3,27 @@ require 'pry-byebug'
 def caesar_cipher(string, shift_factor = 3)
   shift_factor = shift_factor % 26
   string = string.split('')
-  string.each do |char| 
+  encrypted_string = ""
+  string.each do |char|
+    new_char = ''
     if ('A'..'Z').include?(char)
       char_index = char.ord + shift_factor
       if (char_index > 90)
         char_index = char_index - 26
       end
-      puts "#{char_index}"
+      new_char = char_index.chr
     elsif ('a'..'z').include?(char)
       char_index = char.ord + shift_factor
       if (char_index > 122)
         char_index = char_index - 26
       end
-      puts "#{char_index}"
+      new_char = char_index.chr
     else
-      puts "#{char}"
+      new_char = char
     end
+    encrypted_string = encrypted_string + new_char
   end
-  puts "#{string} #{shift_factor}"
+  puts "#{encrypted_string}"
 end
 
 puts "Enter string to encrypt:"
