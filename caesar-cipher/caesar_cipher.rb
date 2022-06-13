5a3ff197abb420bdf1c8cdf296244eaed555f218
Alexander Laudino <93599746+alexander-laudino@users.noreply.github.com>
2022-06-13 16:09:50 -0400

    Normalize shift_factor to proper range and split string by chars

--- caesar-cipher/caesar_cipher.rb
+++ caesar-cipher/caesar_cipher.rb
@@ -1,6 +1,8 @@
 require 'pry-byebug'
 
 def caesar_cipher(string, shift_factor = 3)
+  shift_factor = shift_factor % 26
+  string = string.split('')
   puts "#{string} #{shift_factor}"
 end
 
@@ -8,6 +10,7 @@ puts "Enter string to encrypt:"
 user_string = gets.chomp
 puts "Enter shift factor:"
 user_shift_factor = gets.chomp.to_i
+puts "\n"
 
 #binding.pry
 
