42a461244490bd49e91aa9d2b7b903b7061462df
Alexander Laudino <93599746+alexander-laudino@users.noreply.github.com>
2022-06-13 15:45:58 -0400

    Add caesar_cipher method and test arguments

--- caesar-cipher/caesar_cipher.rb
+++ caesar-cipher/caesar_cipher.rb
@@ -1,8 +1,14 @@
 require 'pry-byebug'
 
+def caesar_cipher(string, shift_factor = 3)
+  puts "#{string} #{shift_factor}"
+end
+
 puts "Enter string to encrypt:"
 user_string = gets.chomp
 puts "Enter shift factor:"
 user_shift_factor = gets.chomp.to_i
 
-binding.pry
+#binding.pry
+
+caesar_cipher(user_string, user_shift_factor)
