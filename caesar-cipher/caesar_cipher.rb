dd154f46e29a74abaf2e7895691fb438a99f0c9d
Alexander Laudino <93599746+alexander-laudino@users.noreply.github.com>
2022-06-13 16:40:52 -0400

    Remove debugger

--- caesar-cipher/caesar_cipher.rb
+++ caesar-cipher/caesar_cipher.rb
@@ -1,5 +1,3 @@
-require 'pry-byebug'
-
 def caesar_cipher(string, shift_factor = 3)
   shift_factor = shift_factor % 26
   string = string.split('')
@@ -31,7 +29,5 @@ user_string = gets.chomp
 puts "Enter shift factor:"
 user_shift_factor = gets.chomp.to_i
 puts "\n"
-
-#binding.pry
 
 caesar_cipher(user_string, user_shift_factor)
