e9d82ed8791967098fd253ce01e14fbed29f079a
Alexander Laudino <93599746+alexander-laudino@users.noreply.github.com>
2022-06-14 05:21:50 -0400

    Complete bubble sort

---
+++ bubble-sort/bubble_sort.rb
@@ -0,0 +1,23 @@
+array = [3,10,4,39,21,15,0,2,42,113]
+
+def bubble_sort(array)
+  array_length = array.size
+  return array if array_length <= 1
+
+  loop do
+    swapped = false
+
+    (array_length-1).times do |i|
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+        swapped = true
+      end
+    end
+
+    break if not swapped
+  end
+
+  array
+end
+
+puts bubble_sort(array)
