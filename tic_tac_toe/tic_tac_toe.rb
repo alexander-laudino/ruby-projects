0e10fdcbbed4aade59273452bbbcff3ac649113c
Alexander Laudino <93599746+alexander-laudino@users.noreply.github.com>
2022-06-15 17:56:43 -0400

    Wrap variable and method in TicTacToe class

--- tic_tac_toe/tic_tac_toe.rb
+++ tic_tac_toe/tic_tac_toe.rb
@@ -1,8 +1,10 @@
-game_board = Array.new(3) { Array.new(3, ' ') }
+class TicTacToe
+  game_board = Array.new(3) { Array.new(3, ' ') }
 
-def print_board(_array)
-  _array.each_with_index do |value, index|
-    puts "#{value[0]}|#{value[1]}|#{value[2]}"
-    puts '-----' if index != 2
+  def print_board(_array)
+    _array.each_with_index do |value, index|
+      puts "#{value[0]}|#{value[1]}|#{value[2]}"
+      puts '-----' if index != 2
+    end
   end
 end
