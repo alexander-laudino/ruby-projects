7bc0bb278c2a52f57d13919530f448cadbbe8569
Alexander Laudino <93599746+alexander-laudino@users.noreply.github.com>
2022-06-15 18:25:36 -0400

    Add initialize method and select_marker method for player_one choice

--- tic_tac_toe/tic_tac_toe.rb
+++ tic_tac_toe/tic_tac_toe.rb
@@ -1,14 +1,37 @@
+require 'pry-byebug'
+
 class TicTacToe
   X = 'X'
   O = 'O'
+  def initialize
+    @players = ['', '']
+    @game_board = Array.new(3) { Array.new(3, ' ') }
+  end
 
-  players = ['', '']
-  game_board = Array.new(3) { Array.new(3, ' ') }
+  def select_marker
+    puts 'Player 1 please choose X or O'
+    loop do
+      player_one = gets.chomp
+      binding.pry
+      if %w[X O].include?(player_one)
+        @players[0] = player_one
+        break
+      else
+        puts 'Incorrect input: Please enter X or O'
+      end
+    end
+  end
 
-  def print_board(_array)
-    _array.each_with_index do |value, index|
+  def print_board
+    @game_board.each_with_index do |value, index|
       puts "#{value[0]}|#{value[1]}|#{value[2]}"
       puts '-----' if index != 2
     end
   end
 end
+
+new_game = TicTacToe.new
+
+new_game.select_marker
+
+new_game.print_board
