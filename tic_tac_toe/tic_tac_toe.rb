7272593e196ced6bc26f207f7b76742369f7edb1
Alexander Laudino <93599746+alexander-laudino@users.noreply.github.com>
2022-06-16 07:32:26 -0400

    Clean up update_winning_combos method

--- tic_tac_toe/tic_tac_toe.rb
+++ tic_tac_toe/tic_tac_toe.rb
@@ -9,8 +9,6 @@ class TicTacToe
     @players = ['', '']
     @game_board = Array.new(3) { Array.new(3, ' ') }
     select_markers
-    play_round
-    play_round
   end
 
   def select_markers
@@ -38,6 +36,7 @@ class TicTacToe
       (1..3).include?(column) ? column -= 1 : next
       @game_board[row][column] = @game_board[row][column] == ' ' ? @players[turn] : next
       print_board
+      update_winning_combos
       break
     end
   end
@@ -45,17 +44,20 @@ class TicTacToe
   def play_round
     puts "Player 1: Place #{@players[0]}"
     select_square
-    binding.pry
-    update_winning_combos
     puts "Player 2: Place #{@players[1]}"
     select_square 1
-    update_winning_combos
   end
 
   def update_winning_combos
-    @winning_combos =
-      [@game_board[0], @game_board[1], @game_board[2],
-       [@game_board[0][0], @game_board[1][0], @game_board[2][0]], [@game_board[0][1], @game_board[1][1], @game_board[2][1]], [@game_board[0][2], @game_board[1][2], @game_board[2][2]], [@game_board[0][0], @game_board[1][1], @game_board[2][2]], [@game_board[0][2], @game_board[1][1], @game_board[2][0]]]
+    top_row = @game_board[0]
+    middle_row = @game_board[1]
+    bottom_row = @game_board[2]
+    first_column = [@game_board[0][0], @game_board[1][0], @game_board[2][0]]
+    second_column = [@game_board[0][1], @game_board[1][1], @game_board[2][1]]
+    third_column = [@game_board[0][2], @game_board[1][2], @game_board[2][2]]
+    diag_one = [@game_board[0][0], @game_board[1][1], @game_board[2][2]]
+    diag_two = [@game_board[0][2], @game_board[1][1], @game_board[2][0]]
+    @winning_combos = [top_row, middle_row, bottom_row, first_column, second_column, third_column, diag_one, diag_two]
   end
 
   private
